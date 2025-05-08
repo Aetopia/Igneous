91c12fe02f43d0fa0aeb3930dd7cbca10c6398f6
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-05-08 17:24:08 +0530

    Format.

--- src/Minecraft.c
+++ src/Minecraft.c
@@ -10,20 +10,20 @@ INIT_ONCE InitOnce = INIT_ONCE_STATIC_INIT;
 
 BOOL InitOnceCallback(PINIT_ONCE InitOnce, PVOID Parameter, PVOID *Context)
 {
-    ExpandEnvironmentStringsW(L"%LOCALAPPDATA%\\Packages\\Microsoft.MinecraftUWP_"
-                              L"8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\minecraftpe\\resource_init_lock",
+    ExpandEnvironmentStringsW(L"%LOCALAPPDATA%\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe"
+                              L"\\LocalState\\games\\com.mojang\\minecraftpe\\resource_init_lock",
                               Release.Path, MAX_PATH);
 
-    ExpandEnvironmentStringsW(L"%LOCALAPPDATA%\\Packages\\Microsoft.MinecraftWindowsBeta_"
-                              L"8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\minecraftpe\\resource_init_lock",
+    ExpandEnvironmentStringsW(L"%LOCALAPPDATA%\\Packages\\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe"
+                              L"\\LocalState\\games\\com.mojang\\minecraftpe\\resource_init_lock",
                               Preview.Path, MAX_PATH);
 
     CoInitializeEx(NULL, COINITBASE_MULTITHREADED | COINIT_DISABLE_OLE1DDE);
 
-    CoCreateInstance(&CLSID_ApplicationActivationManager, NULL,  CLSCTX_INPROC_SERVER,
+    CoCreateInstance(&CLSID_ApplicationActivationManager, NULL, CLSCTX_INPROC_SERVER,
                      &IID_IApplicationActivationManager, (PVOID)&Manager);
 
-    CoCreateInstance(&CLSID_PackageDebugSettings, NULL,  CLSCTX_INPROC_SERVER, &IID_IPackageDebugSettings,
+    CoCreateInstance(&CLSID_PackageDebugSettings, NULL, CLSCTX_INPROC_SERVER, &IID_IPackageDebugSettings,
                      (PVOID)&Settings);
 
     return TRUE;
