52614e49f2b1ad2056e8d4baed2b11453365b8ae
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-05-12 17:46:55 +0530

    Add C++ classes.

--- include/Igneous.h
+++ include/Igneous.h
@@ -58,3 +58,72 @@ extern "C"
 #ifdef __cplusplus
 }
 #endif
+
+#ifdef __cplusplus
+
+#include <optional>
+#include <winrt/base.h>
+
+class Game
+{
+  private:
+    PVOID _This;
+
+  public:
+    Game(PVOID This)
+    {
+        This = _This;
+    }
+
+    bool Installed()
+    {
+        return Game_get_Installed(_This);
+    }
+
+    bool Running()
+    {
+        return Game_get_Running(_This);
+    }
+
+    std::optional<DWORD> Launch()
+    {
+        DWORD dwProcessId = {};
+        HRESULT hResult = Game_Launch(_This, &dwProcessId);
+
+        if (hResult == S_FALSE)
+            return {};
+        else
+            winrt::check_hresult(hResult);
+
+        return dwProcessId;
+    }
+
+    void Debug(bool value)
+    {
+        winrt::check_hresult(Game_set_Debug(_This, value));
+    }
+
+    void Terminate()
+    {
+        winrt::check_hresult(Game_Terminate(_This));
+    }
+};
+
+class Minecraft
+{
+  private:
+    static inline Game _Release = Minecraft_get_Release(), _Preview = Minecraft_get_Preview();
+
+  public:
+    static Game &Release()
+    {
+        return _Release;
+    }
+
+    static Game &Release()
+    {
+        return _Preview;
+    }
+}
+
+#endif
